e381302c4ead433a91046df2220a652b165548f2
Andrew Salcedo <asalcedo07@gmail.com>
2018-01-05 15:05:10 +0100

    fetch books from google api. todo: display books, add search input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
-import './App.css'
+import "./App.css";
+import * as api from './api/api'
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import Header from './components/Header'
+import Header from "./components/Header";
+
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			books: [],
+			text: "Harry Potter"
+		};
+	}
+
+	componentDidMount() {
+		this.getBooks();
+	}
+
+	getBooks = () => {
+    fetch(api.googleUrl + this.state.text)
+      .then(results => {
+        return results.json(); 
+      }).then(data => {
+        this.setState({books: data.items}, () => console.log(this.state))
+      })
+  }
+
 	render() {
 		return (
-      <MuiThemeProvider>
-        <Header />
-      </MuiThemeProvider>
+			<MuiThemeProvider>
+				<Header />
+			</MuiThemeProvider>
 		);
 	}
 }
---
+++ src/api/api.js
@@ -0,0 +1,1 @@
+export const googleUrl = "https://www.googleapis.com/books/v1/volumes?q=";
